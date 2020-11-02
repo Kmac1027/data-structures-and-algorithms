84732cbc9d8560dde2d0ce1911402b2ecf49a091
Kevin McNeil <dogwoodrawks@gmail.com>
2020-11-02 14:02:03 -0800

    fixed a bunch ofs uff

--- code-challenges/tree/README.md
+++ code-challenges/tree/README.md
@@ -1,1 +1,17 @@
 # Trees
+
+## Challenge
+
+Utilize the Single-responsibility principle: any methods you write should be clean, reusable, abstract component parts to the whole challenge. You will be given feedback and marked down if you attempt to define a large, complex algorithm in one function definition.
+
+## Approach & Efficiency
+use recursion within tree classes to create the expected array output
+
+## API
+
+Can successfully instantiate an empty tree
+Can successfully instantiate a tree with a single root node
+Can successfully add a left child and right child to a single root node
+Can successfully return a collection from a preorder traversal
+Can successfully return a collection from an inorder traversal
+Can successfully return a collection from a postorder traversal
--- code-challenges/tree/tree.test.js
+++ code-challenges/tree/tree.test.js
@@ -1,10 +1,42 @@
 'use strict';
-let tree = require('./tree.js');
+let t = require('./tree.js');
 
-describe('Testing to make sure stack and queue are working', () => {
-  test('Can successfully push onto a stack', () => {
-    let testNode = new ll.Stack;
-    testNode.push(5);
-    expect(testNode.top.value).toStrictEqual(5);
+describe('Tree testing', () => {
+  it('should successfully instantiate an empty tree ', () => {
+    let tree = new t.Tree();
+
+    expect(tree.root).toStrictEqual(null);
   });
-});
+  it('should successfully instantiate a tree with a single root node', () => {
+    let tree = new t.BinarySearchTree ();
+    tree.add(1);
+
+
+    expect(tree.root.root).toStrictEqual(1);
+   });
+  it('should successfully add a left child and right child to a single root node', () => {
+    let tree = new t.BinarySearchTree ();
+    tree.add(2);
+    tree.add(3)
+    tree.add(1)
+    expect(tree).toStrictEqual(1);
+  });
+//   it('successfully return a collection from a preorder traversal', () => {
+//     ///
+
+//     ///
+//     expect().toStrictEqual();
+//   });
+//   it('should successfully return a collection from an inorder traversal', () => {
+//     ///
+
+//     ///
+//     expect().toStrictEqual();
+//   });
+//   it('should successfully return a collection from a postorder traversal', () => {
+//     ///
+
+//     ///
+//     expect().toStrictEqual();
+//   });
+ });
