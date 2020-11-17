83091c8be956c8b105c29814032c6b5e9e7d4200
Kevin McNeil <dogwoodrawks@gmail.com>
2020-11-17 13:23:14 -0800

    code challenge 27

--- code-challenges/mergesort/merge-sort.js
+++ code-challenges/mergesort/merge-sort.js
@@ -1,21 +1,17 @@
 'use strict'
 
 function mergeSort (arr) {
-  
   if (arr.length <= 1) {
     return arr;
   }
   const middle = Math.floor(arr.length / 2);
   const left = arr.slice(0, middle);
   const right = arr.slice(middle);
-
   return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge (left, right) {
   let resultArray = [], leftIndex = 0, rightIndex = 0;
-
-  // 
   while (leftIndex < left.length && rightIndex < right.length) {
     if (left[leftIndex] < right[rightIndex]) {
       resultArray.push(left[leftIndex]);
@@ -25,9 +21,7 @@ function merge (left, right) {
       rightIndex++; 
     }
   }
-  return resultArray
-          .concat(left.slice(leftIndex))
-          .concat(right.slice(rightIndex));
+  return resultArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 module.exports = mergeSort
