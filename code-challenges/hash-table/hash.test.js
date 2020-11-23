1eca2968d5384ee68250a49ee5c483222d00679f
Kevin McNeil <dogwoodrawks@gmail.com>
2020-11-22 17:42:33 -0800

    made changes to my code to make it easier and cleaner

--- code-challenges/hash-table/hash.test.js
+++ code-challenges/hash-table/hash.test.js
@@ -4,22 +4,22 @@ let HashTable = require('./hash.js')
 
 describe('Testing Hash Table', () => {
   test('Adding a key/value to your hashtable results in the value being in the data structure', () => {
-    let testTable = new HashTable(1024);
-    testTable.set('Kevin', 'McNeil')
+    let testTable = new HashTable(20);
+    testTable.insert('Kevin', 'McNeil');
 
-    expect(testTable.contains('Kevin')).toStrictEqual(true);
+    expect(testTable.search('Kevin')).toStrictEqual('McNeil');
   });
   test('Retrieving based on a key returns the value stored', () => {
     let testTable = new HashTable(1024);
-    testTable.set('Kevin', 'McNeil')
+    testTable.insert('Kevin', 'McNeil')
 
-    expect(testTable.hash('Kevin')).toStrictEqual(639);
+    expect(testTable.search('Kevin')).toStrictEqual('McNeil');
   });
-  test('Successfully returns null for a key that does not exist in the hashtable', () => {
+  test('Successfully returns null  for a key that does not exist in the hashtable', () => {
     let testTable = new HashTable(1024);
-    testTable.set('Kevin', 'McNeil');
+    testTable.insert('Kevin', 'McNeil');
 
-    expect(testTable.contains('michael')).toStrictEqual(null);
+    expect(testTable.search('michael')).toStrictEqual(null);
   });
   // test('Successfully handle a collision within the hashtable', () => {
 
